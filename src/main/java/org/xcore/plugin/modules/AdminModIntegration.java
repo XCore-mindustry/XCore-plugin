adcd753be3081f730d54d5fd202eea8903c2b1fc
nekonya <senoyabure@gmail.com>
2023-03-26 14:35:53 +0300

    Prepare yourself for probiv nya

--- src/main/java/org/xcore/plugin/modules/AdminModIntegration.java
+++ src/main/java/org/xcore/plugin/modules/AdminModIntegration.java
@@ -1,13 +1,22 @@
 package org.xcore.plugin.modules;
 
+import arc.struct.ObjectSet;
+import arc.struct.Seq;
+import arc.util.Log;
 import arc.util.Time;
 import arc.util.serialization.JsonValue;
+import mindustry.gen.Call;
+import mindustry.io.JsonIO;
+import mindustry.net.Administration;
+import org.xcore.plugin.modules.history.History;
+import org.xcore.plugin.modules.packets.BanInfoPacket;
+import org.xcore.plugin.modules.packets.PlayerInfoPacket;
 import org.xcore.plugin.utils.JavelinCommunicator;
 import org.xcore.plugin.utils.models.BanData;
 
 import java.util.concurrent.TimeUnit;
 
-import static mindustry.Vars.netServer;
+import static mindustry.Vars.*;
 import static org.xcore.plugin.PluginVars.*;
 
 public class AdminModIntegration {
@@ -63,6 +72,35 @@ public class AdminModIntegration {
             ban.generateBid();
 
             JavelinCommunicator.sendEvent(ban);
+        });
+
+        netServer.addPacketHandler("info_request", (player, content) -> {
+            Log.debug("AR "+content);
+            if (player.admin) {
+                ObjectSet<Administration.PlayerInfo> infos = netServer.admins.findByName(content);
+                Seq<PlayerInfoPacket> packets = new Seq<>();
+                infos.each(i->{
+                    packets.add(new PlayerInfoPacket(i.lastName,i.lastIP,i.id,i.admin,i.names,i.ips));
+                });
+                packets.each(p->{
+                    String res = p.generate().toString().replace("\"~","").replace("~\"","").replace("\\t","\"");
+                    Log.debug(res);
+                    Call.clientPacketUnreliable(player.con, "info_data", res);
+                });
+            }
+        });
+
+        netServer.addPacketHandler("bans_request", (player, content) -> {
+            Log.debug("BR "+content);
+            if (player.admin) {
+                Seq<BanData> bans = Database.getBanned(false);
+                Seq<BanInfoPacket> banInfoPackets = new Seq<>();
+                bans.each(b -> banInfoPackets.add(new BanInfoPacket(b.name, b.uuid, b.adminName, b.reason, b.unbanDate)));
+                banInfoPackets.each(p -> {
+                    Log.debug(p.generate().toString());
+                    Call.clientPacketUnreliable(player.con, "ban_data", p.generate().toString());
+                });
+            }
         });
     }
 }
