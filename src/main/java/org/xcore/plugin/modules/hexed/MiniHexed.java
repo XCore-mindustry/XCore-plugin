27d22783c7df36ec1037d47558a4794e0cd85de1
osp54 <clashmekers54@gmail.com>
2023-04-14 18:51:01 +0300

    fix anti-autofill, remove sequential loading of maps

--- src/main/java/org/xcore/plugin/modules/hexed/MiniHexed.java
+++ src/main/java/org/xcore/plugin/modules/hexed/MiniHexed.java
@@ -49,10 +49,7 @@ public class MiniHexed {
         Utils.showLeaderboard(Utils::getHexedLeaderboard);
 
         startBase = Schematics.readBase64("bXNjaAF4nDWQ3W6DMAxGv/wQUpDWV+gLcLPXmXaRQap2YhgFurYvv82ONSLlJLGPbYEWvYNf0lfGy0glny75cdr2VHb0U97Gcl33Ky0Awpw+8rzBvr336Eda11yGe5pndCvd+bzQlBFHWr7zkwqOZypjHtZCn3nc+cFNN0K/0ZzKsKYlsygdh+2SyoR4W2ZKUy7o07UM5yTOE8d72rl2fuylvsBPxDvwivpZ2QyvejZCFy387w+/NUbCXrMaRVCvVSUqDopOICfrOJcXV1TdqG5E94wWrmGwLjio1/0PZAMcC6blG2d6RhTBaqbVTCeZkctFA23rNOAlcKh9uIQXs8a9huVmPcPBWYaXORteFUEmaDQzaJfAcoVVVC+oF9QL6gX5Lx0jdppa5w1S7Q8n5z8n");
-        Vars.netServer.admins.addActionFilter(action -> {
-            if (action.type == ActionType.depositItem && action.block instanceof Turret) return false;
-            return true;
-        });
+        Vars.netServer.admins.addActionFilter(action -> action.type != ActionType.depositItem || !(action.tile.block() instanceof Turret));
         Events.on(EventType.PlayEvent.class, event -> {
             applyRules();
             Timer.schedule(() -> {
