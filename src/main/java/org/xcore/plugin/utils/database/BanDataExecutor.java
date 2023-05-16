f44cf5de410123fef1dd8d168e5e2a8388d353e3
osp54 <clashmekers54@gmail.com>
2023-05-16 18:10:42 +0300

    revert auto unban

--- src/main/java/org/xcore/plugin/utils/database/BanDataExecutor.java
+++ src/main/java/org/xcore/plugin/utils/database/BanDataExecutor.java
@@ -1,17 +1,11 @@
 package org.xcore.plugin.utils.database;
 
 import arc.struct.Seq;
-import arc.util.Log;
-import arc.util.Threads;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.IndexOptions;
 import com.mongodb.client.model.Indexes;
 import com.mongodb.client.model.ReplaceOptions;
-import com.mongodb.client.model.changestream.FullDocumentBeforeChange;
-import com.mongodb.client.model.changestream.OperationType;
 import org.bson.conversions.Bson;
-import org.xcore.plugin.modules.discord.Bot;
-import org.xcore.plugin.utils.SockCommunicator;
 import org.xcore.plugin.utils.models.BanData;
 
 import java.util.concurrent.TimeUnit;
@@ -27,19 +21,6 @@ public class BanDataExecutor {
         this.collection = collection;
 
         collection.createIndex(Indexes.ascending("unbanDate"), new IndexOptions().expireAfter(0L, TimeUnit.MILLISECONDS));
-
-        if (SockCommunicator.isSocketServer()) {
-            Threads.thread(() -> collection.watch()
-                    .fullDocumentBeforeChange(FullDocumentBeforeChange.REQUIRED)
-                    .forEach(event -> {
-                        if (event.getOperationType() != OperationType.DELETE) return;
-
-                        BanData data = event.getFullDocumentBeforeChange();
-                        if (data == null) return;
-
-                        Bot.sendUnban(data);
-                    }));
-        }
     }
 
     public BanData getBan(String uuid, String ip) {
