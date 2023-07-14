6cd407e1bedeb4052e0d65d1f8ae93925fc9ccad
Darkness <inflexibled@gmail.com>
2023-07-14 11:41:18 +0300

    new sock version

--- src/main/java/org/xcore/plugin/utils/NetSock.java
+++ src/main/java/org/xcore/plugin/utils/NetSock.java
@@ -2,14 +2,13 @@ package org.xcore.plugin.utils;
 
 import arc.func.Cons;
 import arc.util.Log;
-import arc.util.Timer;
 import com.ospx.sock.ClientSock;
-import com.ospx.sock.ServerSock;
-import com.ospx.sock.Sock;
 import com.ospx.sock.EventBus.Request;
 import com.ospx.sock.EventBus.RequestSubscription;
 import com.ospx.sock.EventBus.Response;
 import com.ospx.sock.EventBus.Subscription;
+import com.ospx.sock.ServerSock;
+import com.ospx.sock.Sock;
 
 import static org.xcore.plugin.PluginVars.config;
 import static org.xcore.plugin.PluginVars.globalConfig;
@@ -24,14 +23,6 @@ public class NetSock {
         }
 
         safeConnect();
-
-        if (sock.isClient())
-            Timer.schedule(() -> {
-                if (!sock.isConnected()) {
-                    Log.info("Trying reconnect to Sock server");
-                    safeConnect();
-                }
-            }, 0, 120);
     }
 
     public static void safeConnect() {
@@ -43,17 +34,19 @@ public class NetSock {
     }
 
     public static void post(Object event) {
-        if (!sock.isConnected()) return;
-        sock.getBus().fire(event);
+        sock.send(event);
     }
 
     public static <T> Subscription<T> subscribe(Class<T> type, Cons<T> consumer) {
-        return sock.bus.on(type, consumer);
+        return sock.on(type, consumer);
     }
 
     public static <T extends Response> RequestSubscription<T> request(Request<T> request, Cons<T> listener) {
-        if (!sock.isConnected()) return null;
-        return sock.bus.request(request, listener);
+        return sock.request(request, listener);
+    }
+
+    public static <T extends Response> void respond(Request<T> request, T response) {
+        sock.respond(request, response);
     }
 
     public static boolean isSocketServer() {
