944db7b66b17d84787bd53e941d3b2dbc3de0a92
osp54 <76648940+osp54@users.noreply.github.com>
2026-05-24 10:56:40 +0300

    Revert "fix(connect): temporary seq join workaround"
    This reverts commit 3d3a9825c810c75547e464918855c73eec98b9de.

--- src/main/java/org/xcore/plugin/event/net/connect/PlayerConnectionBootstrap.java
+++ src/main/java/org/xcore/plugin/event/net/connect/PlayerConnectionBootstrap.java
@@ -1,22 +1,16 @@
 package org.xcore.plugin.event.net.connect;
 
 import arc.Events;
-import arc.struct.Seq;
-import arc.util.Log;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import mindustry.game.EventType;
-import mindustry.logic.LVar;
 import mindustry.gen.Player;
 import mindustry.net.Administration;
 import mindustry.net.NetConnection;
 import mindustry.net.Packets;
-import mindustry.world.Tile;
-import mindustry.world.blocks.logic.LogicBlock;
 import org.xcore.plugin.session.ObserverService;
 
 import static mindustry.Vars.netServer;
-import static mindustry.Vars.world;
 
 @Singleton
 public class PlayerConnectionBootstrap {
@@ -58,60 +52,8 @@ public class PlayerConnectionBootstrap {
             observerService.restore(player);
         }
 
-        sendWorldData(player);
+        netServer.sendWorldData(player);
 
         Events.fire(new EventType.PlayerConnect(player));
-    }
-
-    private void sendWorldData(Player player) {
-        try {
-            netServer.sendWorldData(player);
-        } catch (IllegalArgumentException ex) {
-            if (!isUnknownSeqType(ex)) {
-                throw ex;
-            }
-
-            int sanitized = sanitizeLogicSeqVariables();
-            if (sanitized <= 0) {
-                throw ex;
-            }
-
-            Log.warn("Sanitized @ logic variables with Seq values before retrying world send for @ (@)",
-                    sanitized, player.plainName(), player.uuid());
-            netServer.sendWorldData(player);
-        }
-    }
-
-    private boolean isUnknownSeqType(IllegalArgumentException ex) {
-        return ex.getMessage() != null && ex.getMessage().contains("Unknown object type: class arc.struct.Seq");
-    }
-
-    static int sanitizeLogicSeqVariables() {
-        if (world == null || world.tiles == null) {
-            return 0;
-        }
-
-        int sanitized = 0;
-
-        for (Tile tile : world.tiles) {
-            if (tile != null && tile.build instanceof LogicBlock.LogicBuild logicBuild) {
-                sanitized += sanitizeLogicSeqVariables(logicBuild.executor.vars);
-            }
-        }
-
-        return sanitized;
-    }
-
-    private static int sanitizeLogicSeqVariables(LVar[] vars) {
-        int sanitized = 0;
-
-        for (LVar var : vars) {
-            if (var != null && var.isobj && var.objval instanceof Seq<?>) {
-                var.objval = null;
-                sanitized++;
-            }
-        }
-
-        return sanitized;
     }
 }
