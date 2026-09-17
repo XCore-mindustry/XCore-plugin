f8f00a653db6bcea3ca98f7e94c6c66038a208c3
osp54 <76648940+osp54@users.noreply.github.com>
2026-09-17 12:36:20 +0300

    chore(release): bump version to 4.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 }
 
 group = "org.xcore.plugin"
-val baseVersion = "4.6.0"
+val baseVersion = "4.7.0"
 version = providers.gradleProperty("xcorePublishVersion").orElse(baseVersion).get()
 val isSnapshotVersion = version.toString().endsWith("-SNAPSHOT")
 val mindustryVersion = libs.versions.mindustry.get()
